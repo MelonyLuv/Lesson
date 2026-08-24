7543f4421369636bf4fe96b4323f874d8914ecba
WinnieChups <winniechups666@gmail.com>
2026-08-24 16:23:58 +0300

    добавлены анимации

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,6 +4,7 @@ namespace Player {
     public class PlayerMovement : MonoBehaviour
     {
         [SerializeField] private float _moveSpeed;
+        [SerializeField] private Animator _animator;
 
         private Vector3 _movement;
 
@@ -19,6 +20,10 @@ namespace Player {
             _movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
 
             transform.position += _movement.normalized * (_moveSpeed * Time.deltaTime);
+
+            _animator.SetFloat("Horizontal", _movement.x);
+            _animator.SetFloat("Vertical", _movement.y);
+            _animator.SetFloat("Speed", _movement.sqrMagnitude);
         }
     }
 
